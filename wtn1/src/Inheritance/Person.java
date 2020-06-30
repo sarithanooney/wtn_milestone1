d4cdf71e206270741485c9cd77100df9f21d943f
sai saritha <sai saritha@Saritha>
2020-06-30 23:19:30 +0530

    Inheritance2

---
+++ wtn1/src/Inheritance/Person.java
@@ -0,0 +1,25 @@
+package Inheritance;
+
+public class Person {
+	
+	
+		private String name;
+		public Person(String n)
+		{
+			name=n;
+		}
+		public void setname(String n)
+		{
+			name=n;
+		}
+		public String getName()
+		{
+			return name;
+		}
+		public String toString()
+		{
+			return name;
+		}
+	}
+
+
---
+++ wtn1/src/Inheritance/TestEmployee.java
@@ -0,0 +1,14 @@
+package Inheritance;
+
+public class TestEmployee {
+	  
+	
+		public static void main(String[] args) 
+		{
+			Person p= new Person("Saritha");
+			Employee e=new Employee("Pooja",10000,2017,"12345");
+			System.out.println(p);
+			System.out.println(e);
+		}
+	}
+
